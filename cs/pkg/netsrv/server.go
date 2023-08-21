2aadde3561a378a02a9d42fc5453e1be2cd804f4
Zombie <iksrotarran.empire@gmail.com>
2023-08-21 18:44:58 +0300

    directory struct and code style fixes

--- cs/pkg/netsrv/server.go
+++ cs/pkg/netsrv/server.go
@@ -1,19 +1,21 @@
 package netsrv
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"strings"
 	"thinknetica/cs/pkg/crawler"
+	"time"
 )
 
 type Server struct {
 	netListener net.Listener
-	handler     func(net.Conn, []crawler.Document)
 	scanResults []crawler.Document
 }
 
-func NewServer(netListener net.Listener, scanResults []crawler.Document, handler func(net.Conn, []crawler.Document)) *Server {
-	return &Server{netListener: netListener, scanResults: scanResults, handler: handler}
+func NewServer(netListener net.Listener, scanResults []crawler.Document) *Server {
+	return &Server{netListener: netListener, scanResults: scanResults}
 }
 
 func (s *Server) ListenAndServe() {
@@ -26,6 +28,52 @@ func (s *Server) ListenAndServe() {
 			continue
 		}
 
-		go s.handler(conn, s.scanResults)
+		go handleClientRequest(conn, s.scanResults)
+	}
+}
+
+func handleClientRequest(connection net.Conn, scanResults []crawler.Document) {
+	defer connection.Close()
+	var err error
+	var needle string
+	reader := bufio.NewReader(connection)
+	searchFunc := func(needle string, scanResults []crawler.Document) []string {
+		var links []string
+		for _, value := range scanResults {
+			if strings.Contains(value.Title, needle) || strings.Contains(value.Body, needle) {
+				links = append(links, fmt.Sprintf("%s %s\n", value.Title, value.Body))
+			}
+		}
+
+		return links
+	}
+
+	for {
+		needle, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Error reading client request:%s \n", err)
+			return
+		}
+
+		needle = strings.TrimSpace(needle)
+		searchResult := searchFunc(needle, scanResults)
+		if len(searchResult) == 0 {
+			result := fmt.Sprintf("No data found by : %s", needle)
+			_, err := connection.Write([]byte(result + "\n"))
+			if err != nil {
+				fmt.Printf("error writing response to client:%s", err)
+			}
+
+			continue
+		}
+
+		result := strings.Join(searchResult, ",")
+		_, err = connection.Write([]byte(result + "\n"))
+
+		if err != nil {
+			fmt.Printf("error writing response to client:%s", err)
+		}
+
+		connection.SetDeadline(time.Now().Add(time.Second * 60))
 	}
 }
