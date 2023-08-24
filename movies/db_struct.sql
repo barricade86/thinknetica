7e0b9a9c959626db678bb4968f46dbd83b36eb22
Kirill Shilov (MediaDirection Group Russia) <Kirill.Shilov@mediadirectiongroup.ru>
2023-08-24 18:44:05 +0300

    movie db implemented

--- movies/db_struct.sql
+++ movies/db_struct.sql
@@ -4,7 +4,10 @@ drop table if exists directors;
 drop table if exists studio;
 drop table if exists movies_and_studios;
 drop table if exists movies_actors_and_directors;
-create table if not exists movies_and_studios(movie_id int not null,studio_id int not null);
+create table if not exists movies_and_studios(
+    movie_id int not null,
+    studio_id int not null
+);
 create unique index movies_and_studios_unq on movies_and_studios (movie_id,studio_id);
 create table movies_actors_and_directors(
     movie_id int not null,
@@ -12,7 +15,9 @@ create table movies_actors_and_directors(
     actor_id int not null
 );
 create unique index movies_actors_and_directors_unq on movies_actors_and_directors (
-    movie_id,director_id,actor_id
+    movie_id,
+    director_id,
+    actor_id
 );
 CREATE TYPE rating AS ENUM ('PG-10', 'PG-13', 'PG-18');
 create table if not exists movies(
@@ -49,35 +54,37 @@ insert into movies(id, title,year,box_office,ratings)
 values(5,'DragonHeart',1996,51367375,'PG-18');
 insert into movies(id, title,year,box_office,ratings) 
 values(6,'A Nightmare on Elm Street Part 2: Freddy''s Revenge',1985,29999213,'PG-18');
---------------------------------------------------------------------------------------------------
+----------------------------------------------------------
 insert into actors(id,name,birth_date) values(1,'Robert Englund','06-06-1947');
 insert into actors(id,name,birth_date) values(2,'Andrew Divoff','07-02-1955');
 insert into actors(id,name,birth_date) values(3,'Dennis Quaid','04-09-1954');
-----------------------------------------------------------------------------------------------------
+----------------------------------------------------------
 insert into directors(id,name,birth_date) values(1,'Wes Craven','08-02-1939');
 insert into directors(id,name,birth_date) values(2,'Robert Kurtzman','11-25-1964');
 insert into directors(id,name,birth_date) values(3,'Jack Sholder','06-08-1945');
 insert into directors(id,name,birth_date) values(4,'Rob Cohen','03-12-1949');
-------------------------------------------------------------------------------------------
+----------------------------------------------------------
 insert into studio(id,title) values(1,'New Line Cinema');
 insert into studio(id,title) values(2,'Universal');
 insert into studio(id,title) values(3,'Live Entertaiment');
------------------------------------------------------------------------------------------
+----------------------------------------------------------
 insert into movies_and_studios(movie_id,studio_id) values(1,1);
 insert into movies_and_studios(movie_id,studio_id) values(2,1);
 insert into movies_and_studios(movie_id,studio_id) values(6,1);
-------------------------------------------------------------------------------------------
+----------------------------------------------------------
 insert into movies_actors_and_directors(movie_id,director_id,actor_id) values(1,1,1);
 insert into movies_actors_and_directors(movie_id,director_id,actor_id) values(3,2,1);
 insert into movies_actors_and_directors(movie_id,director_id,actor_id) values(3,2,2);
 insert into movies_actors_and_directors(movie_id,director_id,actor_id) values(5,4,3);
 insert into movies_actors_and_directors(movie_id,director_id,actor_id) values(6,3,1);
 insert into movies_actors_and_directors(movie_id,director_id,actor_id) values(4,3,2);
--------------------------------------------------------------------------------------------
+----------------------------------------------------------
 select m.id as movieId,m.title as movieName,a.name as actorName 
 from movies m inner join movies_actors_and_directors mad on m.id=mad.movie_id 
-              inner join actors a on mad.actor_id=a.id WHERE a.name='Robert Englund';
+              inner join actors a on mad.actor_id=a.id 
+        WHERE a.name='Robert Englund';
 select * from movies m where m.box_office>1000;                       
 select m.id,m.title,d.name 
 from movies m inner join movies_actors_and_directors mad on m.id=mad.movie_id 
-              inner join directors d on mad.director_id=d.id WHERE d.name='Jack Sholder';
+              inner join directors d on mad.director_id=d.id 
+        WHERE d.name='Jack Sholder';
